f9f29b78e355e195ae6ff7f06b98f1fac3a7b9d6
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-01-22 17:05:03 +0100

    brotli  compression

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require('zlib');
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 if (process.env.NODE_ENV === 'test') {
@@ -119,7 +120,25 @@ module.exports = {
         process.env.FIREBASE_MEASUREMENT_ID
       ),
     }),
-    new CompressionPlugin(),
+    new CompressionPlugin({
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: '[path][base].br',
+      algorithm: 'brotliCompress',
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
   ],
   resolve: {
     extensions: [
